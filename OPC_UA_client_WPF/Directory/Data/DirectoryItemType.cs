264539673a8717c125a0234ea2fdf799bacdaa75
Marek Magáth <xmagat01@vutbr.cz>
2018-02-07 17:24:20 +0100

    refactor to MVVM

---
+++ OPC_UA_client_WPF/Directory/Data/DirectoryItemType.cs
@@ -0,0 +1,21 @@
+﻿namespace OPC_UA_client_WPF
+{
+    /// <summary>
+    /// The type of a directory item
+    /// </summary>
+    public enum DirectoryItemType
+    {
+        /// <summary>
+        /// A logical drive
+        /// </summary>
+        Drive,
+        /// <summary>
+        /// A phyiscal file
+        /// </summary>
+        File,
+        /// <summary>
+        /// A folder
+        /// </summary>
+        Folder
+    }
+}
--- OPC_UA_client_WPF/HeaderToImageConverter.cs
+++ OPC_UA_client_WPF/HeaderToImageConverter.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.IO;
 using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Media.Imaging;
 
 namespace OPC_UA_client_WPF
@@ -9,33 +10,28 @@ namespace OPC_UA_client_WPF
     /// <summary>
     /// Converts a full path to a specific image type of a drive, folder or file
     /// </summary>
-    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    [ValueConversion(typeof(DirectoryItemType), typeof(BitmapImage))]
     class HeaderToImageConverter : IValueConverter
     { 
         public static HeaderToImageConverter Instance = new HeaderToImageConverter();
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // Get the full path
-            var path = (string)value;
-
-            // If the path is null, ignore
-            if (path == null)
-                return null;
-
-            // Get the name of the file/folder
-            var name = MainWindow.GetFileFolderName(path);
-
+             /*       
             // By default, we presume an image
             var image = "Images/file.png";
 
-            // If the name is blank, we presume it's a drive as we cannot have a blank file or folder name
-            if (string.IsNullOrEmpty(name))
-                image = "Images/drive.png";
-            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
-                image = "Images/folder-closed.png";
-
-            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+            switch ((DirectoryItemType)value)
+            {
+                case DirectoryItemType.Drive:
+                    image = "Images/drive.png";
+                    break;
+                case DirectoryItemType.Folder:
+                    image = "Images/folder-closed.png";
+                    break;
+            }
+            */
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/{value}.png"));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
