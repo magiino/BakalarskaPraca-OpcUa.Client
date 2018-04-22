6845b2fe2a43aa34459c742de696e221153aa9a9
Marek Magáth <xmagat01@vutbr.cz>
2018-04-23 01:08:47 +0200

    archive to number converter added

---
+++ OpcUa.Client.WPF/ValueConverters/AtchiveTypeToNumberConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using OpcUa.Client.Core;
+
+namespace OpcUa.Client.WPF
+{
+    public class AtchiveTypeToNumberConverter : BaseValueConverter<AtchiveTypeToNumberConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((ArchiveInterval)value)
+            {
+                case ArchiveInterval.None:
+                    return "0";
+                case ArchiveInterval.TenSecond:
+                    return "10";
+                case ArchiveInterval.ThirtySecond:
+                    return "30";
+                case ArchiveInterval.OneMinute:
+                    return "60";
+                default:
+                    Debugger.Break();
+                    return "0";
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
