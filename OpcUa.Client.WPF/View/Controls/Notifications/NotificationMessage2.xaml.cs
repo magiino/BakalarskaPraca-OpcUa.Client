5fecae9a221a0e42865e0f04ab15ef10701f3977
Marek Magáth <xmagat01@vutbr.cz>
2018-04-23 11:35:58 +0200

    cards added

---
+++ OpcUa.Client.WPF/View/Controls/Notifications/NotificationMessage2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OpcUa.Client.WPF
+{
+    /// <summary>
+    /// Interaction logic for NotificationMessage.xaml
+    /// </summary>
+    public partial class NotificationMessage2 : UserControl
+    {
+        public NotificationMessage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
